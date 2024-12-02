c7ca273861ab086ab298bd53875fbf9aa0d72abc
Marcos Guibson <mguibtech@gmail.com>
2024-12-02 11:01:35 -0400

    [feat] Adding font custom in ios and add font family in text component

--- App.tsx
+++ App.tsx
@@ -6,13 +6,11 @@ function App(): React.JSX.Element {
   return (
     <SafeAreaView>
       <View>
+        <Text>Testando componente </Text>
         <Text style={{color: 'red'}}>Testando componente </Text>
-        <Text preset="headingSmall" style={{color: 'red'}}>
-          Testando componente{' '}
-        </Text>
         <Text
           preset="headingLarge"
-          style={{color: 'red', fontFamily: 'Panchang-Light'}}>
+          style={{color: 'red', fontFamily: 'Panchang-Extralight'}}>
           Testando componente{' '}
         </Text>
       </View>
