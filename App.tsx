4809d80f69932e610d4ef5894a52bce523723ddd
Marcos Guibson <mguibtech@gmail.com>
2024-12-29 10:53:53 -0400

    [feat] Adding basic component of textInput

--- App.tsx
+++ App.tsx
@@ -3,36 +3,35 @@ import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 
 import {Box} from './src/components/Box/Box';
-import {Icon} from './src/components/Icon/Icon';
 
 import {Text} from './src/components/Text/Text';
 import {theme} from './src/theme/theme';
+import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge" italic>
-            Coffstack
+          <Text marginBottom="s8" preset="headingLarge">
+            Ola
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <Box mb="s20">
+            <TextInput label="E-mail" placeholder="Digite o seu e-mail" />
+          </Box>
+          <Box mb="s20">
+            <TextInput label="Senha" placeholder="Digite sua senha" />
+          </Box>
+          <Text color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
           </Text>
 
-          <Box flexDirection="row">
-            <Icon name="chevronRight" size={50} />
-            <Icon name="heartFill" color="buttonPrimary" />
-            <Icon name="profile" size={50} />
-            <Icon name="profileFill" size={50} />
-            <Icon name="heart" size={50} />
-            <Icon name="bellOn" color="carrotSecondary" size={50} />
-          </Box>
-          <Box flexDirection="row">
-            <Icon name="newPost" size={50} />
-            <Icon name="camera" size={50} />
-            <Icon name="chat" size={50} />
-            <Icon name="chatOn" color="error" size={50} />
-            <Icon name="flashOff" size={50} />
-            <Icon name="flashOn" size={50} />
-          </Box>
+          <Button title="Entrar" mt="s48" />
+          <Button title="Criar uma conta" preset="outline" mt="s12" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
