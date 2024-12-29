ffac854e09387399c131b02732bfd892bb81dd3c
Marcos Guibson <mguibtech@gmail.com>
2024-12-28 21:13:49 -0400

    [feat] Adding bibliography icons

--- App.tsx
+++ App.tsx
@@ -1,27 +1,38 @@
+import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 import {SafeAreaView, View} from 'react-native';
+
+import {Box} from './src/components/Box/Box';
+import {Icon} from './src/components/Icon/Icon';
+
 import {Text} from './src/components/Text/Text';
-import {Button} from './src/components/Button/Button';
-import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 
-function App(): React.JSX.Element {
+function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <View>
-          <Text preset="headingMedium">Testando componente </Text>
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" italic>
+            Coffstack
+          </Text>
 
-          <Button
-            title="Testandoo"
-            mb="s10"
-            backgroundColor="carrotSecondary"
-          />
-
-          <Button loading title="Testando" />
-
-          <Button preset="outline" title="Testando" />
-          <Button preset="outline" loading title="Testando" />
+          <Box flexDirection="row">
+            <Icon name="chevronRight" size={50} />
+            <Icon name="heartFill" color="buttonPrimary" />
+            <Icon name="profile" size={50} />
+            <Icon name="profileFill" size={50} />
+            <Icon name="heart" size={50} />
+            <Icon name="bellOn" color="carrotSecondary" size={50} />
+          </Box>
+          <Box flexDirection="row">
+            <Icon name="newPost" size={50} />
+            <Icon name="camera" size={50} />
+            <Icon name="chat" size={50} />
+            <Icon name="chatOn" color="error" size={50} />
+            <Icon name="flashOff" size={50} />
+            <Icon name="flashOn" size={50} />
+          </Box>
         </View>
       </SafeAreaView>
     </ThemeProvider>
