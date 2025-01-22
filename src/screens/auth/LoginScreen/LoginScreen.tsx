640a6def6ebcfefb5dfce03dc3163a5ee83b3bcf
mGuibTech <mguibtech@gmail.com>
2025-01-22 10:58:48 -0400

    [feat] Add email validation and form submission to LoginScreen

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from '../../../components/Text/Text';
-import {Box} from '../../../components/Box/Box';
 import {TextInput} from '../../../components/TextInput/TextInput';
 import {Button} from '../../../components/Button/Button';
 import {Screen} from '../../../components/Screen/Screen';
 import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../../routes/Routes';
+import {Alert} from 'react-native';
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
 
 export function LoginScreen({navigation}: ScreenProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  useEffect(() => {
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    setEmailError(isValidEmail ? '' : 'E-mail inválido');
+  }, [email]);
+
   function navigateToSignUpScreen() {
     navigation.navigate('SignUpScreen');
   }
 
   function navigateToForgotPasswordScreen() {
     navigation.navigate('ForgotPasswordScreen');
+  }
+
+  function submitForm() {
+    Alert.alert('Login', `Email: ${email} - Senha: ${password}`);
   }
 
   return (
@@ -27,11 +40,22 @@ export function LoginScreen({navigation}: ScreenProps) {
       <Text preset="paragraphLarge" mb="s40">
         Digite seu e-mail e senha para entrar
       </Text>
-      <Box mb="s20">
-        <TextInput label="E-mail" placeholder="Digite o seu e-mail" />
-      </Box>
 
-      <PasswordInput label="Senha" placeholder="Digite sua senha" />
+      <TextInput
+        errorMessage={emailError}
+        boxProps={{mb: 's20'}}
+        value={email}
+        onChangeText={setEmail}
+        label="E-mail"
+        placeholder="Digite o seu e-mail"
+      />
+
+      <PasswordInput
+        value={password}
+        onChangeText={setPassword}
+        label="Senha"
+        placeholder="Digite sua senha"
+      />
 
       <Text
         onPress={navigateToForgotPasswordScreen}
@@ -42,7 +66,12 @@ export function LoginScreen({navigation}: ScreenProps) {
         Esqueci minha senha
       </Text>
 
-      <Button title="Entrar" mt="s48" />
+      <Button
+        disabled={!!emailError || password.length <= 4}
+        title="Entrar"
+        mt="s48"
+        onPress={submitForm}
+      />
       <Button
         onPress={navigateToSignUpScreen}
         title="Criar uma conta"
